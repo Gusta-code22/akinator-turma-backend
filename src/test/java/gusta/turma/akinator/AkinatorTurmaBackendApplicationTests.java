96850a93b74b5da9799311ef978b0fabe21d8dab
Gusta-code22 <gustavobritomirandaxbox@gmail.com>
2026-08-23 13:54:30 -0300

    chore: inicializa projeto com Spring Initializr

---
+++ src/test/java/gusta/turma/akinator/AkinatorTurmaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gusta.turma.akinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkinatorTurmaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
